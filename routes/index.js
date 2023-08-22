fb718c9f1bda95f197db34e6661dbfce40ac98ef
Draculakl07 <131325179+Draculakl07@users.noreply.github.com>
2023-08-22 19:21:46 +1000

    latest update part 3

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,46 @@ const itemList = [
   
   {'id':20,'title':'Git',
   'des':'Git is a distributed version control system used to track changes in source code throughout the development process. It enables multiple developers to work simultaneously, managing and merging multiple versions of a project, and providing a history of changes and an efficient way to collaborate.',
-  'ref':'https://git-scm.com/'}
+  'ref':'https://git-scm.com/'},
+  {'id':21,'title':'CircleCI',
+'des':'CircleCI is a continuous integration and delivery platform that automates the build, test, and deployment of software. It provides a modern platform for software teams to improve the speed, stability, and scalability of their CI/CD processes.',
+'ref':'https://circleci.com/'},
+
+{'id':22,'title':'Nexus Repository',
+'des':'Nexus Repository OSS is an open source repository that supports many artifact formats, including Docker, Java™, and npm. With the Nexus tool integration, pipelines in your toolchain can publish and retrieve versioned apps and their dependencies by using central repositories that are accessible from other environments.',
+'ref':'https://www.ibm.com/garage/method/practices/deliver/tool_nexus/'},
+
+{'id':23,'title':'ELK Stack',
+'des':'Its comprised of Elasticsearch, Kibana, Beats, and Logstash (also known as the ELK Stack) and more. Reliably and securely take data from any source, in any format, then search, analyze, and visualize.',
+'ref':'https://www.elastic.co/what-is/elk-stack'},
+
+{'id':24,'title':'Zabbix',
+'des':'Zabbix is an open source monitoring software tool for diverse IT components, including networks, servers, virtual machines (VMs) and cloud services. Zabbix provides monitoring metrics, such as network utilization, CPU load and disk space consumption.',
+'ref':'https://www.techtarget.com/searchitoperations/definition/Zabbix'},
+
+{'id':25,'title':'Rancher',
+'des':'Rancher is an open-source software for delivering Kubernetes-as-a-Service. It helps manage Kubernetes clusters across multiple infrastructures, offering operational and security best practices.',
+'ref':'https://rancher.com/'},
+
+{'id':26,'title':'SonarQube',
+'des':'SonarQube is an open-source platform for continuous inspection of code quality. It provides a detailed report of bugs, code smells, and vulnerabilities in the codebase, helping improve its maintainability and reliability.',
+'ref':'https://www.sonarqube.org/'},
+
+{'id':27,'title':'Spinnaker',
+'des':'Spinnaker is an open-source, multi-cloud continuous delivery platform that helps release software changes with high velocity and confidence. It was created at Netflix and is used by several other large-scale enterprises.',
+'ref':'https://spinnaker.io/'},
+
+{'id':28,'title':'Travis CI',
+'des':'Travis CI is a hosted continuous integration service that provides build and test automation for GitHub projects. It supports multiple programming languages and integrates with many tools to enhance the CI/CD pipeline.',
+'ref':'https://travis-ci.com/'},
+
+{'id':29,'title':'Vagrant',
+'des':'Vagrant is an open-source tool for building and maintaining portable virtual software development environments. It offers a simple way to configure, provision, and manage virtual machines, ensuring consistent environments for development.',
+'ref':'https://www.vagrantup.com/'},
+
+{'id':30,'title':'Consul',
+'des':'HashiCorp Consul is a service networking solution that enables teams to manage secure network connectivity between services and across on-prem and multi-cloud environments and runtimes. Consul offers service discovery, service mesh, traffic management, and automated updates to network infrastructure device.',
+'ref':'https://developer.hashicorp.com/consul/docs/intro'}
   
   
 ];
